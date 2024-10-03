3342ecaeafd328e497ea57a1f55e11448b7e175a
Vitaliy Drozdov <vitdrzdv@mail.ru>
2024-10-03 14:22:37 +0300

    refactor aggregate_multithread.py v2

--- dags/concurrent/aggregate_multithread.py
+++ dags/concurrent/aggregate_multithread.py
@@ -5,6 +5,7 @@ import os
 
 from queue import Queue
 
+# Создание абсолютного пути для импорта:
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(project_root)
 
@@ -28,7 +29,15 @@ SAVE_ABS_PATH = os.path.join(project_root, "data")
 
 
 def run_in_thread(func, *args):
-    """Запускает функцию в отдельном потоке и возвращает поток."""
+    """Запускает func в отдельном потоке.
+
+    Args:
+        func (callable): Функция для выполнения.
+        *args: Параметры функции.
+
+    Returns:
+        tuple: Поток и очередь.
+    """
     q = Queue()
 
     def wrapper():
@@ -45,6 +54,15 @@ NEW_PATH = f"{SAVE_ABS_PATH}\\{INITIAL_FILENAME}_cleaned.csv"
 
 
 def clean_data(path, newpath):
+    """Очищает данные.
+
+    Args:
+        path (str): Путь к исходному файлу.
+        newpath (str): Путь для сохранения очищенного файла.
+
+    Returns:
+        DataFrame: Очищенный датафрейм.
+    """
     return clean(
         filepath=path,
         new_path=newpath,
@@ -52,9 +70,12 @@ def clean_data(path, newpath):
 
 
 def analyze_data():
+    """Выполнение многопоточного скрипта аналогично DAGy."""
+
     clean_data(FILEPATH, NEW_PATH)
     df = read(NEW_PATH)
 
+    # Запуск потоков
     age_thread, _ = run_in_thread(analyze_age, df)
     age_ranges_thread, q_1 = run_in_thread(create_age_ranges, df)
     income_thread, _ = run_in_thread(analyze_income, df)
@@ -65,6 +86,7 @@ def analyze_data():
     )
     occupation_thread, q_3 = run_in_thread(analyze_occupation, df)
 
+    # Ожидание завершения потоков
     threads = [
         age_thread,
         age_ranges_thread,
@@ -76,18 +98,22 @@ def analyze_data():
     for thread in threads:
         thread.join()
 
+    # Получение необходимых данных из очереди:
     age_ranges_res = q_1.get()
     investment_res = q_2.get()
     occupation_res = q_3.get()
 
+    # Поток step:
     step_thread, step_q = run_in_thread(
         step_task, age_ranges_res, occupation_res
     )
     step_thread.join()
     step_path_result = step_q.get()
 
+    # Датафрейм из step:
     df_2 = read(step_path_result)
 
+    # Запуск потоков
     task_4a_thread, _ = run_in_thread(get_occupation_age_group_summary, df_2)
     task_4b_thread, task_4b_q = run_in_thread(
         get_age_income_summary,
@@ -95,17 +121,13 @@ def analyze_data():
         f"{SAVE_ABS_PATH}\\task_4b.csv",
     )
     task_5a_thread, _ = run_in_thread(occupation_ration, df_2)
-    task_5b_thread, tas_5b_q = run_in_thread(
+    task_5b_thread, task_5b_q = run_in_thread(
         get_age_occupation_summary,
         df_2,
         f"{SAVE_ABS_PATH}\\task_5b.csv",
     )
 
-    task_4a_thread.join()
-    task_4b_thread.join()
-    task_5a_thread.join()
-    task_5b_thread.join()
-
+    # Ожидание завершения потоков
     task_threads = [
         task_4a_thread,
         task_4b_thread,
@@ -115,9 +137,11 @@ def analyze_data():
     for thread in task_threads:
         thread.join()
 
+    # Получение данных:
     t_4b = task_4b_q.get()
-    t_5b = tas_5b_q.get()
+    t_5b = task_5b_q.get()
 
+    # Архивирование файлов:
     archive_files(
         [
             step_path_result,
